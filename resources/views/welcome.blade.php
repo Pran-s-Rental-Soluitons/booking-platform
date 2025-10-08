43122c316be3c910d4d6fcc025a989cc85cb22f1
Prathamesh Deokar <deokarprathmesh10@gmail.com>
2025-10-08 18:15:25 +0530

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <h1 class="mt-5 text-center">
+        “India’s First Bus Platform Designed for Women Rentlee”
+    </h1>
+</div>
+@include('includes.footer')
+@endsection
